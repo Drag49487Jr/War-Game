41226df37149d955181b993d0dc0c84c1f297bac
Armando Samperio <armandosamperiomcchs@Armandos-MBP.attlocal.net>
2019-08-05 23:02:59 -0500

    cards in array

--- js/script.js
+++ js/script.js
@@ -1,20 +1,54 @@
-/*----- constants -----*/ 
-var suits = ['s','c','d','h'];
-var ranks = ['02', '03', '04', '05', '06','07','08', '09', '10', 'J', 'Q', 'K', 'A'];
+/*----- constants -----*/
+var suits = ['s', 'c', 'd', 'h'];
+var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
-var mainDeck = buildMainDeck();
-renderDeckContainer(mainDeck,document.getElementById('main-deck-container');)
+// build a 'master' deck of 'card' objects used to create shuffled decks
+var masterDeck = buildMasterDeck();
+//renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
 
-/*----- app's state (variables) -----*/ 
-var shuffledDeck;
+/*----- app's state (variables) -----*/
+var shuffledDeck,playerDeck,cpuDeck;
 
-
-/*----- cached element references -----*/ 
+/*----- cached element references -----*/
 var shuffledContainer = document.getElementById('shuffled-deck-container');
 
-
-/*----- event listeners -----*/ 
-document.querySelector('button').addEventListener('click', renderShuffleDeck);
-
-
+/*----- event listeners -----*/
+document.querySelector('button').addEventListener('click', renderShuffledDeck);
 /*----- functions -----*/
+function renderShuffledDeck() {
+  var tempDeck = masterDeck.slice();
+  playerDeck = [];
+  cpuDeck = [];
+  while (tempDeck.length) {
+    var rndIdx = Math.floor(Math.random() * tempDeck.length);
+    playerDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    cpuDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+
+  }
+  renderDeckInContainer(playerDeck, shuffledContainer);
+}
+
+function renderDeckInContainer(deck, container) {
+  container.innerHTML = '';
+  var cardsHtml = deck.reduce(function(html, card) {
+    return html + `<div class="card ${card.face}"></div>`;
+  }, '');
+  container.innerHTML = cardsHtml;
+}
+
+function buildMasterDeck() {
+  var deck = [];
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        face: `${suit}${rank}`,
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
+}
+
+renderShuffledDeck();
+
+
