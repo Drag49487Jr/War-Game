807250a89eef41adc74ccbd7e1b8777a4510502b
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-08-06 15:36:17 -0500

    added back img

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 /*----- constants -----*/
 var suits = ['spades', 'clubs', 'diamonds', 'hearts'];
-var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-
-// build a 'master' deck of 'card' objects used to create shuffled decks
-//renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
-
+var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14'];
+const playerRender = document.getElementById('playerOne');
+const cpuRender = document.getElementById('computer');
 /*----- app's state (variables) -----*/
 var playerDeck,cpuDeck,shuffledDeck,inPlay,masterDeck;
+
 /*----- cached element references -----*/
 var playerOne = document.getElementById('playerOne');
 var cpu = document.getElementById('computer');
+
 /*----- event listeners -----*/
 document.querySelector('#draw').addEventListener('click', draw);
+
 /*----- functions -----*/
 
 init();
 function shuffleDeck() {
   var tempDeck = masterDeck.slice();
   deck = [];
-  //cpuDeck = [];
   while (tempDeck.length) {
     var rndIdx = Math.floor(Math.random() * tempDeck.length);
     deck.push(tempDeck.splice(rndIdx, 1)[0]);
@@ -32,6 +32,8 @@ function splitDeck() {
 }
 
 function init(){
+    playerRender.innerHTML = `<div class = "card back-red"></div>`
+    cpuRender.innerHTML = `<div class = "card back-red"></div>`
     masterDeck = buildMasterDeck();
     shuffledDeck = shuffleDeck();
     inPlay = [];
@@ -40,6 +42,7 @@ function init(){
 function draw() {
     inPlay.push(playerDeck.pop());
     inPlay.push(cpuDeck.pop());
+
     console.log(inPlay);
     render();
 }
@@ -47,36 +50,41 @@ function draw() {
 function render(){
     playerOne.innerHTML = inPlay[0].value;
     cpu.innerHTML = inPlay[1].value;
-    
+    playerOne.innerHTML = `<div class = "card ${inPlay[0].suit} ${inPlay[0].rank}"></div>`;
+    cpu.innerHTML = `<div class = "card ${inPlay[1].suit} ${inPlay[1].rank}"></div>`;
     renderCompareCards((inPlay[0]),(inPlay[1]));
 }
-
 
 function renderCompareCards(player,computer){
     let pVal = player.value;
     let cVal = computer.value;
-    if (pVal > cVal){
+        if (pVal > cVal){
         playerDeck.unshift(player, computer);
-       // console.log(true);
-    } else if (pVal < cVal){
+            } else if (pVal < cVal){
         cpuDeck.unshift(player, computer);
-        //console.log(false);
-    } else if (pVal === cVal){
+            } else if (pVal === cVal){
+        //initWar();
      console.log('draw');
     }
      inPlay = [];
  }
 
-
-
+/*function initWar(){
+    let playWar = playerDeck.splice();
+    let cpuWar = cpuDeck.splice();
+        inPlay.push();
+        inPlay.pu
+}
+*/
 
 function buildMasterDeck() {
   var deck = [];
   suits.forEach(function(suit) {
     ranks.forEach(function(rank) {
       deck.push({
-        face: `${suit}-r${rank}`,
-        value: Number(rank) || (rank === 'A' ? 11 : 10)
+        suit: `${suit}`,
+        rank: `r${rank}`,
+        value: Number(rank),
       });
     });
   });
